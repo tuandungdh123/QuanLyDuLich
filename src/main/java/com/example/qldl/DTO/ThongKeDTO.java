a2c0d8422483791fa7971d8e80c2ace3b2d385d2
Truong Tuan Dung <tuandungtruong122@gmai.com>
2024-06-19 14:14:25 +0700

    ThongKe

--- src/main/java/com/example/qldl/DTO/ThongKeDTO.java
+++ src/main/java/com/example/qldl/DTO/ThongKeDTO.java
@@ -1,4 +1,19 @@
 package com.example.qldl.DTO;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
 public class ThongKeDTO {
+    private long soLuongRole1;
+    private long soLuongRole2;
+
+    public ThongKeDTO(long soluongRole1, long soluongRole2) {
+        this.soLuongRole1 = soluongRole1;
+        this.soLuongRole2 = soluongRole2;
+
+
+    }
 }
