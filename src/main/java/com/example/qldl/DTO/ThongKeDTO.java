dceca16b2ad6e18feab57bcc9f3a0340fea2c273
Khoi05 <nguynkhoi2004@gmail.com>
2024-06-21 03:34:28 +0700

    NguyenKhoi_commit04: ThongKe Polar Chart

--- src/main/java/com/example/qldl/DTO/ThongKeDTO.java
+++ src/main/java/com/example/qldl/DTO/ThongKeDTO.java
@@ -9,11 +9,22 @@ import lombok.*;
 public class ThongKeDTO {
     private long soLuongRole1;
     private long soLuongRole2;
+    private long tour1;
+    private long tour2;
+    private long tour3;
+    private long tour4;
+    private long tour5;
+
 
     public ThongKeDTO(long soluongRole1, long soluongRole2) {
         this.soLuongRole1 = soluongRole1;
         this.soLuongRole2 = soluongRole2;
-
-
+    }
+    public ThongKeDTO(long tour1, long tour2, long tour3, long tour4, long tour5) {
+        this.tour1 = tour1;
+        this.tour2 = tour2;
+        this.tour3 = tour3;
+        this.tour4 = tour4;
+        this.tour5 = tour5;
     }
 }
--- src/main/java/com/example/qldl/Service/Implement/ThongKeImpl.java
+++ src/main/java/com/example/qldl/Service/Implement/ThongKeImpl.java
@@ -2,7 +2,9 @@ package com.example.qldl.Service.Implement;
 
 import com.example.qldl.DTO.ThongKeDTO;
 import com.example.qldl.Entity.AccountEntity;
-import com.example.qldl.Repository.ThongKeRepo;
+import com.example.qldl.Entity.TourE;
+import com.example.qldl.Repository.AccountRepo;
+import com.example.qldl.Repository.TourRepo;
 import com.example.qldl.Service.ThongKeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,11 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class ThongKeImpl implements ThongKeService {
-    private final ThongKeRepo thongKeRepo;
+    private final AccountRepo accountRepo;
+    private final TourRepo tourRepo;
     @Override
     public ThongKeDTO countRole() {
-        List<AccountEntity> accounts = thongKeRepo.findAll();
+        List<AccountEntity> accounts = accountRepo.findAll();
 
         if (accounts.isEmpty()) {
             return new ThongKeDTO(0, 0);
@@ -29,5 +32,34 @@ public class ThongKeImpl implements ThongKeService {
                 .count();
 
         return new ThongKeDTO(soLuongRole1, soLuongRole2);
+    }
+
+    @Override
+    public ThongKeDTO countTour() {
+        List<TourE> tours= tourRepo.findAll();
+        if (tours.isEmpty()) {
+            return new ThongKeDTO(0, 0,0,0,0);
+        }
+        long soLuongTour1 = tours.stream()
+                .filter(tour -> tour.getTypeTourE().getType_Id() == 1)
+                .count();
+
+        long soLuongTour2 = tours.stream()
+                .filter(tour -> tour.getTypeTourE().getType_Id() == 2)
+                .count();
+
+        long soLuongTour3 = tours.stream()
+                .filter(tour -> tour.getTypeTourE().getType_Id() == 3)
+                .count();
+
+        long soLuongTour4 = tours.stream()
+                .filter(tour -> tour.getTypeTourE().getType_Id() == 4)
+                .count();
+
+        long soLuongTour5 = tours.stream()
+                .filter(tour -> tour.getTypeTourE().getType_Id() == 5)
+                .count();
+
+        return new ThongKeDTO(soLuongTour1,soLuongTour2,soLuongTour3,soLuongTour4,soLuongTour5);
     }
 }
